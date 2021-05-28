3b91837ad941545f1a27d2d953297600194a04d8
Julie <julie.pierrisnard@gmail.com>
2021-05-28 18:58:36 -0400

    integrated code

--- main.py
+++ main.py
@@ -1,1 +1,210 @@
-print("hello world")
+import numpy as np
+from plot import *
+
+##################################################################################################################
+#generate char_map of locations
+
+# Number of rows and columns TODO: Change to USER INPUT
+row_num = 5
+col_num = 6
+
+char_map = []
+# Create Randomized Map
+for i in range(row_num):
+    row = []
+    for j in range(col_num):
+        rd = random.randint(1, 4)
+        if rd == 1:
+            row.append("p")
+        elif rd == 2:
+            row.append("q")
+        elif rd == 3:
+            row.append("v")
+        else:
+            row.append("n")
+    char_map.append(row)
+
+print(char_map)
+
+#display map to screen
+displayMap(char_map, row_num, col_num)
+
+##################################################################################################################
+#generate an array of points pointArr for path finding
+
+pointArr_row = row_num +1
+pointArr_col = col_num +1
+
+nbPoints = pointArr_row * pointArr_col
+pointList = []
+
+for i in range(nbPoints):
+	pointList.append(i+1)
+
+tempPointArr = np.asarray(pointList)
+pointArr = np.reshape(tempPointArr, (pointArr_row, pointArr_col))
+
+print("array of points:")
+print(pointArr)
+
+##################################################################################################################
+#PEEKING FUNCTIONS
+#checks if the index is in bounds of an array
+#takes an array and a point (i,j)
+def isInBounds(arr, index):
+	row, col = arr.shape
+	if index[0] >= 0 and index[0] < row and index[1] >= 0 and index[1] < col:
+		return True
+	else:
+		return False
+
+
+#peeks right and returns the cost of moving right
+#moving right along an edge requires us to fetch the locations at the top and at the bottom of the edge and returning the average
+#takes a point (i,j)
+def peekRight(origin):
+	dest = (origin[0], origin[1]+1)
+	if(isInBounds(pointArr, dest)): #make sure the place we want to go exists first
+		top = (origin[0]-1, origin[1])
+		bot = (origin[0], origin[1])
+		if not isInBounds(locArr, top):
+			#DEBUG: print('returning bot')
+			return locArr[bot]
+		if not isInBounds(locArr, bot):
+			#DEBUG: print('returning top')
+			return locArr[top]
+		#DEBUG: print('returning average')
+		return (locArr[top] + locArr[bot]) /2
+	else:
+		pass
+
+
+#peeks left and returns the cost of moving left
+#moving left along an edge requires us to fetch the locations at the top and at the bottom of the edge and returning the average
+#takes a point (i,j)
+def peekLeft(origin):
+	dest = (origin[0], origin[1]-1)
+	if(isInBounds(pointArr, dest)): #make sure the place we want to go exists first
+		top = (dest[0]-1, dest[1])
+		bot = (dest[0], dest[1])
+		if not isInBounds(locArr, top):
+			#DEBUG: print('returning bot')
+			return locArr[bot]
+		if not isInBounds(locArr, bot):
+			#DEBUG: print('returning top')
+			return locArr[top]
+		#DEBUG: print('returning average')
+		return (locArr[top] + locArr[bot]) /2
+	else:
+		pass
+
+
+#peeks up and returns the cost of moving up
+#moving up along an edge requires us to fetch the locations at the left and at the right of the edge and returning the average
+#takes a point (i,j)
+def peekUp(origin):
+	dest = (origin[0]-1, origin[1])
+	if(isInBounds(pointArr, dest)): #make sure the place we want to go exists first
+		left = (dest[0], dest[1]-1)
+		right = (dest[0], dest[1])
+		if not isInBounds(locArr, left):
+			#DEBUG: print('returning right')
+			return locArr[right]
+		if not isInBounds(locArr, right):
+			#DEBUG: print('returning left')
+			return locArr[left]
+		#DEBUG: print('returning average')
+		return (locArr[left] + locArr[right]) /2
+	else:
+		pass
+
+
+#peeks down and returns the cost of moving down
+#moving down along an edge requires us to fetch the locations at the left and at the right of the edge and returning the average
+#takes a point (i,j)
+def peekDown(origin):
+	dest = (origin[0]+1, origin[1])
+	if(isInBounds(pointArr, dest)): #make sure the place we want to go exists first
+		left = (origin[0], origin[1]-1)
+		right = (origin[0], origin[1])
+		if not isInBounds(locArr, left):
+			#DEBUG: print('returning right')
+			return locArr[right]
+		if not isInBounds(locArr, right):
+			#DEBUG: print('returning left')
+			return locArr[left]
+		#DEBUG: print('returning average')
+		return (locArr[left] + locArr[right]) /2
+	else:
+		pass
+
+
+##################################################################################################################
+#DIJKSTRA ALGORITHM????
+#start at beginning and put the first point in the visited list
+#explore all neighbour points
+#update the total distance of the points explored if the total distance is smaller than their current value
+#visit the point with the lowest distance
+#repeat
+#end when we find our destination
+
+visitedPoints = {} #dictionary containing all the points that have been visited already
+dist = {} #dictionary containing all the explored  points and their distances (initially infinity??)
+
+
+#visits a point with the lowest distance by adding removing it from dist{} and adding it to visitedPoints{}
+def visitMinDist():
+	pass
+
+
+#explores neighbour points and updates their distance value in dist{}
+def exploreNeighbours(origin):
+	point_right = (origin[0], origin[1]+1)
+	cost_right = peekRight(origin)
+	if (cost_right is not None and point_right not in visitedPoints): #make sure the cost of going right isn't None and the point has not already been visited
+		total_cost_right = visitedPoints[origin] + cost_right  #the distance is cumulative
+		updateDistance(point_right, total_cost_right)
+
+	point_left = (origin[0], origin[1]-1)
+	cost_left = peekLeft(origin)
+	if (cost_left is not None and point_left not in visitedPoints):
+		total_cost_left = visitedPoints[origin] + cost_left
+		updateDistance(point_left, total_cost_left)
+
+	point_up = (origin[0]-1, origin[1])
+	cost_up = peekUp(origin)
+	if (cost_up is not None and point_up not in visitedPoints):
+		total_cost_up = visitedPoints[origin] + cost_up
+		updateDistance(point_up, total_cost_up)
+
+	point_down = (origin[0]+1, origin[1])
+	cost_down = peekDown(origin)
+	if (cost_down is not None and point_down not in visitedPoints):
+		total_cost_down = visitedPoints[origin] + cost_down
+		updateDistance(point_down, total_cost_down)
+
+
+#updates distance value in dist{} if it is lower than the current one
+def updateDistance(point, newValue):
+	if point in dist : #if the point is already explored then we just compare the value
+		if newValue > dist[point]:
+			dist[point] = newValue
+	else: #otherwise the point was not previously explored so we add it to dist{}
+		dist[point] = newValue
+
+
+#testing dictionaries
+# test = {(1, 2): 10, (3, 0): 100}
+# print(test)
+# if (1, 2) in test:
+# 	print('heck yeah')
+# #adding new point and value
+# test[(0,1)] = 400
+# print(test)
+# #updating value
+# test[(0,1)] = 60
+# print(test[(0,1)])
+
+
+##################################################################################################################
+#HEURISTIC FUNCTIONS
--- plot.py
+++ plot.py
@@ -4,91 +4,74 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib.cm as cm
 
-# Color Values
-color_vals = [
+
+def displayMap(char_map, row_num, col_num):
+    # Color Values
+    color_vals = [
     ["quarantine", 1],
     ["playground", 2],
     ["vaccine", 3],
     ["neutral", 4]
-]
+    ]
+  
+    width = 0.2  # cell width
+    height = 0.1  # cell height
+    
+    # Create color map for UI purpose only
+    color_map = []
+    for i in range(row_num):
+        row = []
+        for j in range(col_num):
+            if char_map[i][j] == "p":
+                row.append(color_vals[0][1])
+            elif char_map[i][j] == "q":
+                row.append(color_vals[1][1])
+            elif char_map[i][j] == "v":
+                row.append(color_vals[2][1])
+            else:
+                row.append(color_vals[3][1])
+        color_map.append(row)
+    color_map = np.array(color_map)
 
-# Number of rows and columns TODO: Change to USER INPUT
-row_num = 5
-col_num = 6
-width = 0.2  # cell width
-height = 0.1  # cell height
+    plt.figure(figsize=(10, 5))
+    plt.subplots_adjust(left=0.1, right=0.8, top=0.9, bottom=0.1)
+    im = plt.imshow(color_map, extent=[0, col_num * width, 0, row_num * height], cmap=cm.tab20c, aspect='auto',
+                    interpolation='none')
 
-char_map = []
-# Create Randomized Map
-for i in range(row_num):
-    row = []
-    for j in range(col_num):
-        rd = random.randint(1, 4)
-        if rd == 1:
-            row.append("p")
-        elif rd == 2:
-            row.append("q")
-        elif rd == 3:
-            row.append("v")
-        else:
-            row.append("n")
-    char_map.append(row)
+    # Draw legends
+    # colormap used by imshow
+    colors = [im.cmap(im.norm(value[1])) for value in color_vals]
+    # create a patch (proxy artist) for every color
+    patches = [mpatches.Patch(color=colors[i], label=color_vals[i][0]) for i in range(len(color_vals))]
+    # put those patched as legend-handles into the legend
+    plt.legend(handles=patches, loc=4, bbox_to_anchor=(1.27, 0), prop={'size': 12}, borderaxespad=0.3)
 
-# Create color map for UI purpose only
-color_map = []
-for i in range(row_num):
-    row = []
-    for j in range(col_num):
-        if char_map[i][j] == "p":
-            row.append(color_vals[0][1])
-        elif char_map[i][j] == "q":
-            row.append(color_vals[1][1])
-        elif char_map[i][j] == "v":
-            row.append(color_vals[2][1])
-        else:
-            row.append(color_vals[3][1])
-    color_map.append(row)
-color_map = np.array(color_map)
+    # create indexes shown on plot
+    index_markers = []
+    for i in range(row_num+1):
+        for j in range(col_num+1):
+            text = "{i},{j}".format(i=i, j=j)
+            x_coord = j * width  # x coordinate
+            y_coord = (row_num - i) * height  # y coordinate
+            plt.text(x_coord, y_coord, text,
+                     ha="center", va="center", bbox=dict(boxstyle=f"circle,pad=0.2", fc="white"))
 
-plt.figure(figsize=(10, 5))
-plt.subplots_adjust(left=0.1, right=0.8, top=0.9, bottom=0.1)
-im = plt.imshow(color_map, extent=[0, col_num * width, 0, row_num * height], cmap=cm.tab20c, aspect='auto',
-                interpolation='none')
+    # make the grids according to the cell sizes
+    plt.xticks(np.arange(0, col_num * width, width))
+    plt.yticks(np.arange(0, row_num * height, height))
+    plt.grid(color='white', linestyle='-.', linewidth=3)
 
-# Draw legends
-# colormap used by imshow
-colors = [im.cmap(im.norm(value[1])) for value in color_vals]
-# create a patch (proxy artist) for every color
-patches = [mpatches.Patch(color=colors[i], label=color_vals[i][0]) for i in range(len(color_vals))]
-# put those patched as legend-handles into the legend
-plt.legend(handles=patches, loc=4, bbox_to_anchor=(1.27, 0), prop={'size': 12}, borderaxespad=0.3)
+    # Draw the Paths TODO: Provide a function
+    x2, y2 = [0.0, 0.2], \
+             [0.3, 0.3]
+    plt.plot(x2, y2, 'r', marker='o', linewidth=4)
 
-# create indexes shown on plot
-index_markers = []
-for i in range(row_num+1):
-    for j in range(col_num+1):
-        text = "{i},{j}".format(i=i, j=j)
-        x_coord = j * width  # x coordinate
-        y_coord = (row_num - i) * height  # y coordinate
-        plt.text(x_coord, y_coord, text,
-                 ha="center", va="center", bbox=dict(boxstyle=f"circle,pad=0.2", fc="white"))
+    x2, y2 = [0.2, 0.2], \
+             [0.3, 0.2]
+    plt.plot(x2, y2, 'r', marker='o', linewidth=4)
 
-# make the grids according to the cell sizes
-plt.xticks(np.arange(0, col_num * width, width))
-plt.yticks(np.arange(0, row_num * height, height))
-plt.grid(color='white', linestyle='-.', linewidth=3)
+    x2, y2 = [0.2, 0.4], \
+             [0.2, 0.2]
+    plt.plot(x2, y2, 'r', marker='o', linewidth=4)
 
-# Draw the Paths TODO: Provide a function
-x2, y2 = [0.0, 0.2], \
-         [0.3, 0.3]
-plt.plot(x2, y2, 'r', marker='o', linewidth=4)
-
-x2, y2 = [0.2, 0.2], \
-         [0.3, 0.2]
-plt.plot(x2, y2, 'r', marker='o', linewidth=4)
-
-x2, y2 = [0.2, 0.4], \
-         [0.2, 0.2]
-plt.plot(x2, y2, 'r', marker='o', linewidth=4)
-
-plt.show()
+    plt.show()
